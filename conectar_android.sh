249d92760d5d14204b5e46390c2f350015b6f7c4
anderson <andersonbuitron@unicauca.edu.co>
2018-05-30 03:23:25 -0500

    adicion de volcado de registro de llamadas

--- conectar_android.sh
+++ conectar_android.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 CONTROL=0
 
-
 #PS1='\[$(tput setaf 1)(\t)$(tput sgr0)\][\u-\W]\$> '
 while [ $CONTROL -eq 0 ] ; do
 	cat /etc/mtab | grep media >> /dev/null
@@ -45,19 +44,34 @@ while [ $CONTROL -eq 0 ] ; do
 			echo "hash md5 - Finalización `date +"%x - %X"`"
 						
 			adb forward tcp:8888 tcp:8888
-			adb shell su < trasferencia_imagen.sh >> /dev/null &
-			sleep 5
-			nc 127.0.0.1 8888 | bar | cat > $UBICACION_MUSB_CONECTADO"/"device_image.dd
+			###adb shell su < trasferencia_imagen.sh >> /dev/null &
+			###sleep 5
+			###nc 127.0.0.1 8888 | bar | cat > $UBICACION_MUSB_CONECTADO"/"device_image.dd
 		
 			echo -e "\e[0;33m" 
-			cat $UBICACION_MUSB_CONECTADO"/"device_image.dd |  bar | md5sum  
+			###cat $UBICACION_MUSB_CONECTADO"/"device_image.dd |  bar | md5sum  
 			echo -e "\e[0m" 
 			
 			#echo -e 'Así se escribe \e[1;34mG\e[0m\e[1;31mo\e[0m\e[1;33mo\e[0m\e[1;34mg\e[0m\e[1;32ml\e[0m\e[1;31me\e[0m'
 			
 			#https://www.cgsecurity.org/wiki/Scripted_run#Command_list
-			photorec /log /d $UBICACION_MUSB_CONECTADO"/"recuperados /cmd device_image.dd partition_none,options,mode_ext2,fileopt,everything,enable,search
-
+			###photorec /log /d $UBICACION_MUSB_CONECTADO"/"recuperados /cmd device_image.dd partition_none,options,mode_ext2,fileopt,everything,enable,search
+			# obtener registro de llamadas
+			
+			adb shell su < trasferencia_registro.sh >> /dev/null &
+			sleep 4
+			nc 127.0.0.1 8888 | bar | cat > $UBICACION_MUSB_CONECTADO"/"logs.db
+			sleep 2
+			registro1=`echo "ls /data/data/com.android.providers.contacts/databases/contacts2.db" | adb shell su &` &
+			echo "$registro1 --------------"
+			if [ echo $registro1 | wc -l  -eq 0 ]; then
+				echo "existe /data/data/com.android.providers.contacts/databases/contacts2.db"; 
+				else echo "no existe";
+			fi
+			sleep 2
+			
+			sqlite3  -header -csv $UBICACION_MUSB_CONECTADO"/"logs.db "select _id, number,duration,name,date from logs;" | cat >> $UBICACION_MUSB_CONECTADO"/"registro.csv
+			sqlite3  -header -csv $UBICACION_MUSB_CONECTADO"/"logs.db "select _id, number,duration,name,date from calls;" | cat >> $UBICACION_MUSB_CONECTADO"/"registro.csv
 		fi
 		
 		read -n 1 -s -r -p 'Press any key to continue';
